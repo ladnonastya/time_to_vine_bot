0f35d337d8dcff16fe2cebff0efdeab46d3501fe
Anastasiya Kharitonova <anastasiya52@mail.ru>
2017-12-16 01:12:06 +0300

    'try_23'

--- time_to_vine_bot.py
+++ time_to_vine_bot.py
@@ -66,27 +66,33 @@ def main():
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            #today += 1
+            apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)            
 
         elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
             greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            #today += 1
+            apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)            
 
         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
             greet_bot.send_message(last_chat_id, 'Доброго вечерочка, {}'.format(last_chat_name))
-            #today += 1
+            apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)    
             
         elif last_chat_text.lower() in greetings and today == now.day and (23 <= hour or hour < 6) :
             greet_bot.send_message(last_chat_id, 'Доброй ночи, {}'.format(last_chat_name))
+            apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)			
 
         elif last_chat_text.lower()=='го бухать' or last_chat_text.lower()=='го бухать?' or last_chat_text.lower()=='го бухать!':
-            #greet_bot.send_message(last_chat_id, 'Го')
+            greet_bot.send_message(last_chat_id, 'ну го, чё')
             apihelper.send_photo(token,last_chat_id,'http://picscomment.com/pics/3690.jpg')
+            apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)
+            last_chat_inline_command = last_update['callback_query']['data']
+            last_chat_text = last_update['callback_query']['message']['text']
+            last_chat_id = last_update['callback_query']['message']['chat']['id']
+            last_chat_name = last_update['callback_query']['message']['chat']['first_name']
+            if last_chat_inline_command == 'vinishko':
+                greet_bot.send_message(last_chat_id, 'я думаю, что тебе нужно винишко, {}'.format(last_chat_name))
+            elif last_chat_inline_command == 'tequila':
+                greet_bot.send_message(last_chat_id, 'для тебя сейчас самое оно - текила!, {}'.format(last_chat_name))
             
-        
-        
-        apihelper.send_message(token, last_chat_id, 'Как твои дела сегодня?', reply_markup = m)
-        
         new_offset = last_update_id + 1
 
 if __name__ == '__main__':  
