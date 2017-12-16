ad940a8ae15a554e2ea27e835d22edecd19dafcf
Anastasiya Kharitonova <anastasiya52@mail.ru>
2017-12-16 13:35:22 +0300

    'try_44'

--- time_to_vine_bot.py
+++ time_to_vine_bot.py
@@ -80,32 +80,34 @@ def main():
                 last_chat_text = last_update['message']['text']
                 last_chat_id = last_update['message']['chat']['id']
                 #last_chat_name = last_update['message']['chat']['first_name']
-                last_chat_username = last_update['message']['chat']['username']
-                if last_chat_username=='pabaev':
-                    last_chat_name='Павел Ованесович'
-                elif last_chat_username=='kislorod8':
-                    last_chat_name='Ленка'
-                elif last_chat_username=='katyko':
-                    last_chat_name='Екатерина Владимировна'
-                elif last_chat_username=='ladno_nastya':
-                    last_chat_name='Хозяйка'
-                else:
-                    last_chat_name = last_update['message']['chat']['first_name']
+                if 'last_chat_username' in last_update['message']['chat'].keys():
+                    last_chat_username = last_update['message']['chat']['username']
+                        if last_chat_username=='pabaev':
+                            last_chat_name='Павел Ованесович'
+                        elif last_chat_username=='kislorod8':
+                            last_chat_name='Ленка'
+                        elif last_chat_username=='katyko':
+                            last_chat_name='Екатерина Владимировна'
+                        elif last_chat_username=='ladno_nastya':
+                            last_chat_name='Хозяйка'
+                        else:
+                            last_chat_name = last_update['message']['chat']['first_name']
             elif 'callback_query' in last_update.keys():
                 last_chat_text = last_update['callback_query']['message']['text']
                 last_chat_id = last_update['callback_query']['message']['chat']['id']
                 #last_chat_name = last_update['callback_query']['message']['chat']['first_name']
-                last_chat_username = last_update['callback_query']['message']['chat']['username']
-                if last_chat_username=='pabaev':
-                    last_chat_name='Павел Ованесович'
-                elif last_chat_username=='kislorod8':
-                    last_chat_name='Ленка'
-                elif last_chat_username=='katyko':
-                    last_chat_name='Екатерина Владимировна'
-                elif last_chat_username=='ladno_nastya':
-                    last_chat_name='Хозяйка'
-                else: 
-                    last_chat_name = last_update['callback_query']['message']['chat']['first_name']
+                if 'last_chat_username' in last_update['message']['chat'].keys():
+                    last_chat_username = last_update['message']['chat']['username']
+                        if last_chat_username=='pabaev':
+                            last_chat_name='Павел Ованесович'
+                        elif last_chat_username=='kislorod8':
+                            last_chat_name='Ленка'
+                        elif last_chat_username=='katyko':
+                            last_chat_name='Екатерина Владимировна'
+                        elif last_chat_username=='ladno_nastya':
+                            last_chat_name='Хозяйка'
+                        else: 
+                            last_chat_name = last_update['callback_query']['message']['chat']['first_name']
                     
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
